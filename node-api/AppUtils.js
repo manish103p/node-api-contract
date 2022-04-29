549367304cb292233906bbe9797bdde99cf55d0c
manish103p <manish.parihar@somaiya.edu>
2022-04-29 19:27:31 +0530

    ccp updates

--- node-api/AppUtils.js
+++ node-api/AppUtils.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 exports.buildCCPOrg = () => {
 	// load the common connection configuration file
-	const ccpPath = path.resolve(__dirname, '..', 'peer', 'cert', 'ccp.json');
+	const ccpPath = path.resolve(__dirname, '..', '..', 'peer', 'cert', 'ccp.json');
 	const fileExists = fs.existsSync(ccpPath);
 	if (!fileExists) {
 		throw new Error(`no such file or directory: ${ccpPath}`);
